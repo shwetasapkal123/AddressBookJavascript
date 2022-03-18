480fed07d2d3ce5e53b6abbddb998fffd876c085
shwetasapkal123 <sapkalshweta30797@gmail.com>
2022-03-18 10:36:25 +0530

    [shweta] validating added contacts using regex and throw error

--- AddressBookJavascript.js
+++ AddressBookJavascript.js
@@ -45,21 +45,66 @@ function AddContact(firstName, lastName, address, city, state, zipCode, phoneNum
 //Function to get the details of the contact from the user
 function getContactDetails() {
     try {
+        //UC2-Regex patterns for validating contact details
+        let namePattern = new RegExp('^[A-Z]{1}[a-z]{2,}$');
+        let addressPattern = new RegExp('^[A-Za-z]{4,}$');
+        let zipCodePattern = new RegExp('^[1-9]{1}[0-9]{2}[ ]?[0-9]{3}$');
+        let phoneNumPattern = new RegExp('^\\+?91[ ]?[1-9][0-9]{9}$');
+        let emailIdPattern = new RegExp('^[a-zA-Z0-9]{3,}([._+-][0-9a-zA-Z]{2,})*@[0-9a-zA-Z]+[.]?([a-zA-Z]{2,4})+[.]?([a-zA-Z]{2,3})*$');
+        //Validating first name
         let firstName = readlineSync.question('Enter Your FirstName : ');
+        if (namePattern.test(firstName))
+        this.namePattern=firstName;
+        else
+            throw 'First name should have minimum 3 characters';
+        //Validating last name
         let lastName = readlineSync.question('Enter Your LastName : ');
+        if (namePattern.test(lastName))
+        this.namePattern=lastName;
+        else
+            throw 'last name should have minimum 3 characters';
+        //Validating address
         let address = readlineSync.question('Enter Your Address : ');
+        if (!addressPattern.test(address))
+            throw 'Address should have minimum 4 characters';
+        //Validating city
         let city = readlineSync.question('Enter Your City Name: ');
+        if (addressPattern.test(city))
+        this.addressPattern=city;
+        else
+            throw 'City should have minimum 4 characters';
+        //Validating states
         let state = readlineSync.question('Enter Your State Name : ');
+        if (addressPattern.test(state))
+        this.addressPattern=state;
+        else
+            throw 'State should have minimum 4 characters';
+        //Validating zip code
         let zipCode = parseInt(readlineSync.question('Enter Your Zip Code : '));
+        if (zipCodePattern.test(zipCode))
+        this.zipCodePattern=zipCode;
+        else
+            throw 'Zipcode is not valid';
+        //Validating phone number
         let phoneNumber = parseInt(readlineSync.question('Enter Your Phone Number : '));
+        if (phoneNumPattern.test(phoneNumber))
+        this.phoneNumPattern=phoneNumber;
+        else
+            throw 'Phone number is not valid';
+        //Validating email id
         let emailId = readlineSync.question('Enter Your Email Id : ');
+        if (emailIdPattern.test(emailId))
+        this.emailIdPattern=emailId;
+        else
+            throw 'Email id is not valid';
         let contactDetails = AddContact(firstName, lastName, address, city, state, zipCode, phoneNumber, emailId);
-        console.log("\nDetails Of Contacts");
+        console.log("\nContact Details: ");
         console.log(contactDetails.toString());
     } catch (e) {
         console.error(e);
     }
 }
+
 
 //Calling the function
 getContactDetails();
